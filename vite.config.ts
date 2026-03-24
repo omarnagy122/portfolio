250f004576daeac00eaeeb3dea5df35facb86762
Omar A. Nagy <oa1819@fayoum.edu.eg>
2026-03-24 11:14:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // التعديل هنا: خليناها نقطة واحدة بس عشان يقرأ من الفولدر الحالي مهما كان اسمه
-  base: "./", 
+  // التعديل الأساسي هنا: اسم الـ Repo الجديد
+  base: "/portfolio/", 
   server: {
     host: "::",
     port: 8080,
