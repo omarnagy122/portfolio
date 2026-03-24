0c9a8abccf294a2d9e292dc996a11491313ef405
Omar A. Nagy <oa1819@fayoum.edu.eg>
2026-03-24 11:05:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // التعديل هنا: خليناها نقطة واحدة بس عشان يقرأ من الفولدر الحالي مهما كان اسمه
+  base: "./", 
   server: {
     host: "::",
     port: 8080,
